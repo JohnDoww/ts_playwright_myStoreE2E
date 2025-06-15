f04ddf48d437c8acd8e9a37cd4d2e44cfa4bd711
JohnDoww <andreyprotsenko99@gmail.com>
2025-06-15 17:23:51 +0200

    updated the method name

--- app/components/AddedItemModal.component.ts
+++ app/components/AddedItemModal.component.ts
@@ -1,15 +1,11 @@
-import { Page, Locator } from "@playwright/test";
+import { Locator } from "@playwright/test";
 import { BaseComp } from "./Base.component";
 
 export class AddedItemModal extends BaseComp {
-  readonly title: Locator = this.page.locator("#blockcart-modal.in");
+  private title: Locator = this.page.locator("#blockcart-modal.in");
   private closeBtn: Locator = this.page.locator("#blockcart-modal .close");
 
-  constructor(page: Page) {
-    super(page);
-  }
-
-  async clickOnClose() {
+  async clickOnCloseBtn() {
     await this.closeBtn.waitFor({ state: "visible" });
     await this.closeBtn.click();
   }
